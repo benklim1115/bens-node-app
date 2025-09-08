f6e56f3dbab5639ed181cc919cf245c76f90d5cd
Ben Klimchock <klimmy1115@gmail.com>
2025-09-08 14:10:08 -0500

    Fix

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello from Docker! 🐳</h1>');
+  res.end('<h1>Hello from Docker!</h1>');
 });
 
 server.listen(8080, () => {

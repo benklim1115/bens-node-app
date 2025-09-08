ccbf0f6dc2fc1faad4d34cd0e7c03e229e3bb44b
Ben Klimchock <klimmy1115@gmail.com>
2025-09-08 13:57:23 -0500

    Updating app.js

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 /** Node Js Tutorial */
-console.log("Uh Howdy??");
+// console.log("Uh Howdy??");
 
-const newParagraph = document.createElement("p");
-newParagraph.innerHTML = "This is a test too! <strong>-_0</strong>";
+// const newParagraph = document.createElement("p");
+// newParagraph.innerHTML = "This is a test too! <strong>-_0</strong>";
 
-document.body.append(newParagraph);
+// document.body.append(newParagraph);
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello from Docker! 🐳</h1>');
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
